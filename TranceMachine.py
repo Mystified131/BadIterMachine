619c64319f2e7c279884d0b39bd24b0ad97ae5fc
Thomas Park <mystifiedthomas@gmail.com>
2021-02-21 16:58:36 -0600

    Tweaking

--- TranceMachine.py
+++ TranceMachine.py
@@ -33,7 +33,7 @@ for subdir, dirs, files in os.walk(srchstr):
             if int(os.stat(filepath).st_size) < sizlim:
                 contentbeats.append(filepath)
 
-        if  filepath.endswith(".wav") and ("Sequence" in str(filepath))  and ("Trance" in str(filepath)) :
+        if  filepath.endswith(".wav") and ("Synth" in str(filepath))  and ("Trance" in str(filepath)) :
             if int(os.stat(filepath).st_size) < sizlim:  
                 contentdrones.append(filepath)
 
@@ -45,7 +45,7 @@ for subdir, dirs, files in os.walk(srchstr):
             if int(os.stat(filepath).st_size) < sizlim:  
              contentorg.append(filepath)
 
-        if  filepath.endswith(".wav") and ("Synth" in str(filepath)) and ("Trance" in str(filepath)) :
+        if  filepath.endswith(".wav") and ("Drone" in str(filepath)) and ("Low" in str(filepath)) :
             if int(os.stat(filepath).st_size) < sizlim:  
                 contentsax.append(filepath)
 
